30c5ee4567593031eaafee47e05fd01eeedf6f08
zoharai <zohar.aizenbud@gmail.com>
2016-01-24 17:57:25 +0200

    update Variable

--- nonogramSolver/src/solver/csp/Variable.java
+++ nonogramSolver/src/solver/csp/Variable.java
@@ -1,19 +1,37 @@
 package solver.csp;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-;
+import java.util.TreeSet;
+
 
 /**
  * Created by Zohar on 24/01/2016.
  */
 public class Variable {
-    private int start;
+    private Integer start;
     private int length;
     private boolean isRow;
     private int index;
     private Set<Integer> legalValues; // The possible indexes of start.
     private List<Constraint> constraints;
+
+    public Variable(int length, boolean isRow, int index, int maxIndex){
+        this.start = null;
+        this.length = length;
+        this.isRow = isRow;
+        this.index = index;
+        constraints = new ArrayList<Constraint>();
+        legalValues = new TreeSet<Integer>();
+        for (int i=0; i<maxIndex; i++){
+            legalValues.add(i);
+        }
+    }
+
+    public void addConstraint(Constraint constraint){
+        constraints.add(constraint);
+    }
 
     /**
      * Returns the value of the array in the domain.
