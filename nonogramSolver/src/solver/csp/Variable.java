f3507d6efc5baff30edc6ae3ca7688f5a10da903
tmrlvi <tmrlvi@gmail.com>
2016-01-24 17:25:28 +0200

    Added get-value for variables

--- nonogramSolver/src/solver/csp/Variable.java
+++ nonogramSolver/src/solver/csp/Variable.java
@@ -14,4 +14,14 @@ public class Variable {
     private int index;
     private Set<Integer> legalValues; // The possible indexes of start.
     private List<Constraint> constraints;
+
+    /**
+     * Returns the value of the array in the domain.
+     * In our case, this is just a number. In fact - just the start location
+     * of the block
+     * @return the value of the variable - the start location.
+     */
+    public int getStartValue(){
+        return start;
+    }
 }
